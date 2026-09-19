00a364c3b9a4235339a646388f4ae80f631a27b8
Nandan Timsina <nandantimsina@gmail.com>
2026-09-19 15:48:35 +0530

    Initial commit

---
+++ src/main/java/fraud_scoring_engine/FraudScoringEngineApplication.java
@@ -0,0 +1,13 @@
+package fraud_scoring_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudScoringEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudScoringEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fraud_scoring_engine/FraudScoringEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package fraud_scoring_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudScoringEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
